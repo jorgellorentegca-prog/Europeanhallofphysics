7795a5725ba883d5e1a5d4fb5e7de9f17d840c0a
jorgellorentegca-prog <jorge.llorentegca@gmail.com>
2026-02-17 18:18:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,8 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist"),
+    assetsDir: "public",
     emptyOutDir: true,
   },
 });
